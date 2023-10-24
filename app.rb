7bd96c1146388af97a495e3e01853097d9e2d36e
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-10-24 19:08:54 +0000

    error

--- app.rb
+++ app.rb
@@ -9,4 +9,14 @@ puts "Looking up weather condition in #{user_location}..."
 gmaps_key = ENV.fetch("GMAPS_KEY")
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=" + gmaps_key
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
+
+raw_map_response = HTTP.get(gmaps_url)
+
+parsed_map_response = JSON.parse(raw_map_response)
+
+results_array = parsed_map_response.fetch("results")
+
+address = results_array.at(2)
+
+puts address
