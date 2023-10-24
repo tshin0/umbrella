d2914f89cf67308a7289857d41d4144dcb972a8b
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-10-24 18:49:13 +0000

    secret key added

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require "http"
 require "json"
 
 puts "Will you need umbrella today?"
-
 puts "Where are you?"
+user_location = gets.chomp
+puts "Looking up weather condition in #{user_location}..."
 
 gmaps_key = ENV.fetch("GMAPS_KEY")
-
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=707leahyredwoodcity&key=" + gmaps_key
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=" + gmaps_key
